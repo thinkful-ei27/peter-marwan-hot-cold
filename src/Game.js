1f4c8fd3e74daa5e3fb508a2a6bd89b3b30c1247
Marwan Alaloosi <manoalosi@gmail.com>
2019-01-22 16:27:54 -0600

    added top nav without styling

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './Game.css';
+import TopNav from './components/nav'
 
 class Game extends Component {
   render() {
     return (
       <section className="App">
-        <Header />
+        <TopNav></TopNav>
       </section>
     );
   }
---
+++ src/components/nav.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+
+export default function TopNav(){
+
+    return <div className="top-nav">
+    <nav><ul>
+        <li><a href="#">WHAT ?</a></li>
+        <li><a href="#">+ NEW GAME</a></li>
+        </ul>
+        </nav>
+    </div>
+}
