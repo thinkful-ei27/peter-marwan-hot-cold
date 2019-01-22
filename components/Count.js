24aa10657658261c15db29832172576ee5a6e477
Peter Ramos <peterdramos02@gmail.com>
2019-01-22 17:01:21 -0500

    remove {Component} from stateless functional components

--- components/Count.js
+++ components/Count.js
@@ -1,5 +1,5 @@
 // This component will be responsible for counting our guesses and displaying them on the page
-import React, { Component } from 'react';
+import React from 'react';
 
 export default function Count () {
 
--- components/Form.js
+++ components/Form.js
@@ -1,1 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+export default function Form () {
+
+
+}
