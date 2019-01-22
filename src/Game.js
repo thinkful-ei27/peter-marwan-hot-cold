a1a5b2ea19108c915ab7c9acdd883eab7cd2a311
Peter Ramos <peterdramos02@gmail.com>
2019-01-22 16:08:40 -0500

    add components dir

---
+++ src/Game.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Game.css';
+
+class Game extends Component {
+  render() {
+    return (
+      <section className="App">
+
+      </section>
+    );
+  }
+}
+
+export default Game;
